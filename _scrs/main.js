c80b4e697fe2a4f7eb2cd9230b5bb0d152b9bd9b
Dmitry Osmakov <bumshmyak@yandex.ru>
2014-08-29 19:40:39 +0200

    changed questions

--- _scrs/main.js
+++ _scrs/main.js
@@ -1,44 +1,28 @@
-var CATEGORIES = [ 'FRACTIONS TO DECIMALS', 
-					'DECIMALS TO FRACTIONS', 
-					'GCF', 
-					'LCF', 
-					'PROPORTIONS' ]; 
+
+
+var CATEGORIES = [ 'Фауна', 
+					'Игры', 
+					'Еда и напитки', 
+					'Литература и искусство' ]; 
 
 var OPTIONS = [ 
 				// first row 
-				'<sup>5</sup>&frasl;<sub>8</sub> to decimal', 
-				'.2 to fraction', 
-				'GFC of 21 and 49', 
-				'LCF of 6 and 14', 
-				'15/18', 
+				'О каком втором домашнем любимце мечтают молодожены?',
+				'По какой настольной игре жених устраивал турнир, на котором сам же и занял 1е место?', 
+				'Какое блюдо жених требует приготовить ему каждые выходные? ', 
+				'Собрание сочинений какого автора подарил жених невесте на одну из годовщин?', 
 
 				// second row 
-				'<sup>46</sup>&frasl;<sub>8</sub> to decimal', 
-				'.58 to fraction', 
-				'GCF of 13 and 7', 
-				'LCM of 8 and 12', 
-				'6/7', 
+				'Что больше всего любит подъедать кот молодоженов  (кроме мяса)?', 
+				'Как называется игровое мероприятие, участвуя в котором жених случайно перезнакомился почти со всеми родственниками невесты?', 
+				'Чем в августе пришлось запастись молодоженам на полгода вперед?', 
+				'Какую книгу перечитывал жених 3 раза за последний год? ', 
 
 				// third row 
-				'3 <sup>2</sup>&frasl;<sub>5</sub> to decimal', 
-				'2.14 to fraction', 
-				'GCF of 20, 16, and 36', 
-				'LCM of 15 and 18', 
-				'9/8', 
-
-				// fourth row 
-				'Name a technique that can be used to convert fractions to decimals', 
-				'Name a technique that can be used to convert decimals to fractions', 
-				'State the definition of GFC', 
-				'State the definition of LCM', 
-				'What does a proportion consist of?', 
-
-				// fifth row 
-				'In Mr. Brown\'s class, <sup>2</sup>&frasl;<sub>3</sub> of the students are wearing red shirts and <sup>1</sup>&frasl;<sub>4</sub> are wearing blue shirts. What fraction of the students does this make all together? ', 
-				'Henry found 0.18 cents in his wallet and found another 0.78 cents in his car. What is the total amount that Henry found? Convert the total to a fraction.', 
-				'Regan is preparing sales kits for her sales representatives to show to customers. She has 20 samples of tile and 12 samples of carpet, and she wants to create identical sales kits, with no samples left over. What is the greatest number of sales kits Regan can prepare?', 
-				'Martin is buying AA batteries and D batteries. The store sells AA batteries in packs of 3 and D batteries in packs of 8. If Martin wishes to buy the same number of AA and D batteries, what is the smallest number of each battery type that he can buy?', 
-				'Do these ratios form a proportion?<br><br>2 men to 20 women<br>3 men to 19 women'
+				'Назовите полное имя кота.', 
+				'На турнире по какой дисциплине провели молодожены свой первый совместный отпуск в сентябре 2010', 
+				'В какой стране жених впервые отведал самое вкусное пиво в его жизни? ', 
+				'Произведениями какого композитора любит попугивать жених невесту?', 
 
 				]; 
 
@@ -46,11 +30,11 @@ $(document).ready(function() {
 	var w = window.innerWidth; 
 	var h = window.innerHeight; 
 
-	for(var i = 0; i<30; i++) {
+	for(var i = 0; i < 16; i++) {
 		var c = ' class="option"'; 
-		var l = Math.floor(i/5)*100; 
+		var l = Math.floor(i/4)*100; 
 		var d = ' id="i'+i+'"'; 
-		if(i<5) {
+		if(i<4) {
 			c = ' class="category"'; 
 			l = CATEGORIES[i]; 
 		}
@@ -61,19 +45,19 @@ $(document).ready(function() {
 	$('body').append('<box></box>'); 
 
 	$('block').css({ 
-		'width':((w/5)-60)+'px', 
-		'height':((h/6)-50)+'px', 
+		'width':((w/5)-40)+'px', 
+		'height':((h/5)-40)+'px', 
 	}); 
 
 	$('block.option').on('click',function() {
 		$(this).attr('class','off'); 
 		var id = $(this).attr('id'); 
-		$('box').fadeIn().html('<table width="100%" height="100%" border="0"><tr><th>'+OPTIONS[parseInt(id.substring(1,id.length+1))-5]+'</th></tr></table>'); 
+		$('box').fadeIn().html('<table width="100%" height="100%" border="0"><tr><th>'+OPTIONS[parseInt(id.substring(1,id.length+1))-4]+'</th></tr></table>'); 
 	}); 
 
 	$('box').css({
-		'width':(w-60)+'px', 
-		'height':(h-60)+'px' 
+		'width':(w-40)+'px', 
+		'height':(h-40)+'px' 
 	}).hide(); 
 	$('box').on('click', function() {
 		$(this).hide(); 
